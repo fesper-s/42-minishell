764e4deb86f63a20fd423b88cb580a5ff401997e
Guga-melo <gustavosoaresgsaf@gmail.com>
2022-12-28 17:12:12 -0300

    add_history and exit function added

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gussoare <gussoare@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 14:04:41 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/12/28 17:01:28 by gussoare         ###   ########.fr       */
+/*   Updated: 2022/12/28 17:11:27 by gussoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	char	*cmd;
+
 	(void) argv;
 	(void) envp;
 	ignore_signal();
@@ -21,7 +23,11 @@ int	main(int argc, char **argv, char **envp)
 		return (print_error("This program do not accept arguments\n"));
 	while (1)
 	{
-		readline("minishell % ");
+		cmd = readline("minishell % ");
+		if (cmd)
+			add_history(cmd);
+		if (ft_strncmp(cmd, "exit", 4) == 0)
+			break ;
 	}
 	return (0);
 }
